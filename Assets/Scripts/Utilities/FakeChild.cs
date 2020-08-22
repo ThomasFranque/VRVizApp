fed1d61d81b18c2114fc62fa0c4ff9b5935a3ef6
Thomas the Franque <tfranque@gmail.com>
2020-08-22 11:19:35 +0100

    Add VR Inputs

---
+++ Assets/Scripts/Utilities/FakeChild.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+// From https://answers.unity.com/questions/518399/simulate-child-parent-relationship.html
+public class FakeChild : MonoBehaviour
+{
+    [SerializeField] private Transform _fakeParent = default;
+    // If true, will attempt to scale the child accurately as the parent scales
+    // Will not be accurate if starting rotations are different or irregular
+    // Experimental
+    [SerializeField, Tooltip("If true, will attempt to scale the child accurately as the parent scales. " +
+        "Will not be accurate if starting rotations are different or irregular Experimental ")]
+    private bool attemptChildScale = false;
+
+    Vector3 startParentPosition;
+    Quaternion startParentRotationQ;
+    Vector3 startParentScale;
+
+    Vector3 startChildPosition;
+    Quaternion startChildRotationQ;
+    Vector3 startChildScale;
+
+    Matrix4x4 parentMatrix;
+
+    void Start()
+    {
+        if (_fakeParent != default)
+        {
+            Init(_fakeParent, false);
+        }
+    }
+
+    public void Init(Transform parent, bool forceSameForward)
+    {
+        _fakeParent = parent;
+        startParentPosition = _fakeParent.position;
+        startParentRotationQ = _fakeParent.rotation;
+        startParentScale = _fakeParent.lossyScale;
+
+        startChildPosition = transform.position;
+        startChildRotationQ = transform.rotation;
+        startChildScale = transform.lossyScale;
+
+        // Keeps child position from being modified at the start by the parent's initial transform
+        startChildPosition = DivideVectors(Quaternion.Inverse(_fakeParent.rotation) * (startChildPosition - startParentPosition), startParentScale);
+        UpdateAction = UpdateChild;
+        if (forceSameForward) UpdateAction += ForceForward;
+    }
+
+    public void Stop()
+    {
+        UpdateAction = default;
+    }
+
+    private void UpdateChild()
+    {
+        parentMatrix =
+            Matrix4x4.TRS(_fakeParent.position, _fakeParent.rotation, _fakeParent.lossyScale);
+
+        transform.position = parentMatrix.MultiplyPoint3x4(startChildPosition);
+
+        transform.rotation =
+            (_fakeParent.rotation *
+                Quaternion.Inverse(startParentRotationQ)) *
+            startChildRotationQ;
+
+        // Incorrect scale code; it scales the child locally not gloabally; Might work in some cases, but will be inaccurate in others
+        if (attemptChildScale)
+            transform.localScale = Vector3.Scale(startChildScale,
+                DivideVectors(_fakeParent.lossyScale, startParentScale));
+    }
+
+    private void ForceForward()
+    {
+        transform.forward = _fakeParent.forward;
+    }
+
+    void LateUpdate()
+    {
+        UpdateAction?.Invoke();
+    }
+
+    Vector3 DivideVectors(Vector3 num, Vector3 den) =>
+        new Vector3(num.x / den.x, num.y / den.y, num.z / den.z);
+
+    private Action UpdateAction;
+}
---
+++ Assets/Scripts/Wheel/VR/Input/SpawnListener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRWheel.VR.Input
+{
+    public class SpawnListener : MonoBehaviour
+    {
+        [SerializeField]
+        private string _targetObjectName = default;
+
+        // Update is called once per frame
+        void Update()
+        {
+            GameObject found = GameObject.Find(_targetObjectName);
+            if (found != null) OnFound(found);
+        }
+
+        private void OnFound(GameObject found)
+        {
+            // transform.position = found.transform.position;
+            // transform.forward = found.transform.forward;
+            // gameObject.AddComponent<FakeChild>().Init(found.transform, true);
+            transform.SetParent(found.transform);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            Destroy(this);
+        }
+    }
+}
