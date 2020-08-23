024474625cd3e91298bd5c9a8e0ad8283916e175
Thomas the Franque <tfranque@gmail.com>
2020-08-23 12:44:36 +0100

    Finalize inputs and fix search window

--- Assets/Scripts/Wheel/Individuals/Grabber.cs
+++ Assets/Scripts/Wheel/Individuals/Grabber.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -10,6 +11,9 @@ namespace VRWheel
     public class Grabber : MonoBehaviour
     {
         [SerializeField] private float _animationIntensity = 1.5f;
+        [SerializeField] private Transform _pointerTarget = default;
+        [SerializeField] private Transform _dragTarget = default;
+        private float _distance = 1.5f;
         private Wheel _wheel;
         private Button _button;
         private Vector3 _initialScale;
@@ -19,6 +23,11 @@ namespace VRWheel
             _button.onClick.AddListener(CloseAll);
             _wheel = wheel;
             _initialScale = transform.localScale;
+        }
+
+        private void LateUpdate()
+        {
+            UpdateAction?.Invoke();
         }
 
         public void Expand()
@@ -33,9 +42,27 @@ namespace VRWheel
             transform.DOScale(_initialScale / 1.5f, .3f);
         }
 
+        public void StartFollow()
+        {
+            UpdateAction = FollowPointerTarget;
+        }
+
+        public void EndFollow()
+        {
+            UpdateAction = default;
+        }
+
+        private void FollowPointerTarget()
+        {
+            Vector3 targetPos = _pointerTarget.position + (_pointerTarget.forward * _distance);
+            _dragTarget.position = Vector3.Lerp(_dragTarget.position, targetPos, 0.1f);
+        }
+
         private void CloseAll()
         {
             _wheel.CloseAll();
         }
+
+        Action UpdateAction;
     }
 }
---
+++ Assets/Scripts/Wheel/VR/ConstantLookat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRWheel.VR
+{
+    public class ConstantLookat : MonoBehaviour
+    {
+        public Transform _target = default;
+
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            transform.LookAt(_target);
+            transform.forward = - transform.forward;
+        }
+    }
+}
